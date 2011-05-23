7e4e39e6563343b79684f668bd929e8156f32085
Fehmi Can SAĞLAM <fehmican.saglam@4primes.com>
2011-05-24 02:29:41 +0300

    son degisiklikler

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,11 +9,34 @@ import models.*;
 
 public class Application extends Controller {
 
+	@Before
+	static void beforeAction() {
+		Logger.info("before action: %1$s", request.action);
+	}
+
+	@After
+	static void afterAction() {
+		Logger.info("after action: %1$s", request.action);
+	}
+
+	@Catch(NullPointerException.class)
+	static void exception(Throwable t) {
+		Logger.error("bir hata oluştu");
+	}
+
+	@Finally
+	static void finalMethod() {
+		Logger.info("final method");
+	}
+
 	public static void index() {
 		if (User.count() == 0) {
 			new User("test", "Play Framework").save();
 		}
 		List users = User.findAll();
+		Role role = new Role();
+		role.name = "test_role";
+		System.out.println("role name: " + role.name);
 		render(users);
 	}
 
